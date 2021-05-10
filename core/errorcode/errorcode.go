f5c71e9cb0d9a7c518c1914f4d4ba8aad90b76f9
brick <0x036d@gmail.com>
2021-05-10 19:33:54 +0800

    add errorcode

---
+++ core/errorcode/errorcode.go
@@ -0,0 +1,38 @@
+package errorcode
+
+import (
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"strconv"
+)
+
+type ErrorCode struct {
+	Code    int
+	Message string
+}
+
+func From(e ErrorCode) error {
+	return GrpcStatusError(e.Code, e.Message)
+}
+
+func GrpcStatusError(code int, msg string) error {
+	st := status.New(codes.Unknown, "")
+	br := &errdetails.ErrorInfo{Metadata: map[string]string{"code": strconv.Itoa(code), "message": msg}}
+	st, _ = st.WithDetails(br)
+	return st.Err()
+}
+func Convert(err error) (int32, string) {
+	s := status.Convert(err)
+	var c int
+	var m string
+	for _, detail := range s.Details() {
+		switch t := detail.(type) {
+		case *errdetails.ErrorInfo:
+			data := t.GetMetadata()
+			c, _ = strconv.Atoi(data["code"])
+			m = data["message"]
+		}
+	}
+	return int32(c), m
+}
