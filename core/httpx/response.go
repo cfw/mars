157895e0df84655c5a08c4c6ebd253ad199ebefd
brick <0x036d@gmail.com>
2021-05-21 10:40:21 +0800

    add httpx OkWithData function

--- core/httpx/response.go
+++ core/httpx/response.go
@@ -10,6 +10,14 @@ func Ok() *Response {
 	return new(Response)
 }
 
+func OkWithData(data interface{}) *Response {
+	resp := Ok()
+	if data != nil {
+		resp.Data = data
+	}
+	return resp
+}
+
 func Error(c int, m string) *Response {
 	return &Response{Code: c, Message: m}
 }
