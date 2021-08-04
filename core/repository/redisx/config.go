7073fe8c70da70524afa141446b861c3c02c4c47
brick <0x036d@gmail.com>
2021-08-04 16:41:13 +0800

    redis pool size

--- core/repository/redisx/config.go
+++ core/repository/redisx/config.go
@@ -3,9 +3,11 @@ package redisx
 import "fmt"
 
 type Config struct {
-	Host     string `yaml:"host"`
-	Port     int    `yaml:"port"`
-	Database int    `yaml:"database"`
+	Host        string `yaml:"host"`
+	Port        int    `yaml:"port"`
+	Database    int    `yaml:"database"`
+	PoolSize    int    `yaml:"poolSize"`
+	MinIdleConn int    `yaml:"minIdleConn"`
 }
 
 func (r *Config) Url() string {
--- core/repository/redisx/redis.go
+++ core/repository/redisx/redis.go
@@ -8,8 +8,10 @@ import (
 
 func NewRedis(c *Config) *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr: c.Url(),
-		DB:   c.Database,
+		Addr:         c.Url(),
+		DB:           c.Database,
+		PoolSize:     c.PoolSize,
+		MinIdleConns: c.MinIdleConn,
 	})
 	log.Info("Connected to Redis")
 	if err := client.Ping(context.TODO()).Err(); err != nil {
